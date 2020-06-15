7b807523c6c0758786ece0b1a0ac40ae442f0ede
Vytautas <tumas.vytautas@gmail.com>
2020-06-15 13:53:20 +0300

    Daugiau pakeitimu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+let colour = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+let spalva = "#123456";
+let fonas = document.getElementById("fonas");
+fonas.addEventListener("click", function(){
+    spalva = "#";
+    for(let i = 0; i < 6; i++){
+        spalva += colour[Math.floor(Math.random()*colour.length)];
+    };
+    console.log(spalva);
+    
+   document.body.style.backgroundColor = spalva; 
+});
+
+
 let MC = document.querySelector(".cursor");
 let MC2 = document.querySelector(".cursor2");
 let L = document.querySelectorAll(".klase");
@@ -15,11 +29,36 @@ function cursor(e){
 
 
 
-L.forEach(link => {
-    link.addEventListener("mouseleave", () => {
-        MC2.classList.remove("link-grow");
-    });
-    link.addEventListener("mouseover", () => {
-        MC2.classList.add("link-grow");
-    });
+//L.forEach(link => {
+//    link.addEventListener("mouseleave", () => {
+//        MC2.classList.remove("link-grow");
+//    });
+//    link.addEventListener("mouseover", () => {
+//        MC2.classList.add("link-grow");
+//    });
+//});
+
+let btnP = document.getElementById("btnP");
+let btnR = document.getElementById("btnR");
+let btnN = document.getElementById("btnN");
+let foto = ["img/1.jpg","img/2.jpg","img/3.jpg","img/4.jpg"];
+let counter = 0;
+
+let mygtukaiPRN = document.querySelector(".mygtukaiPRN");
+let myImg = document.getElementById("myImg");
+
+btnN.addEventListener("click", function(){
+    counter++;
+    myImg.src = foto[counter];
+    if(counter == foto.length){counter=0;}
+});
+btnP.addEventListener("click", function(){
+    counter--;
+    myImg.src = foto[counter];
+    if(counter < 0){counter=foto.length - 1;}
+});
+btnR.addEventListener("click", function(){
+    counter=Math.floor(Math.random()*foto.length);
+    myImg.src = foto[counter];
+    console.log(counter);
 });
